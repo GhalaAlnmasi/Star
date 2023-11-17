8861fd40624955d843d6d50045bbc1714dcfd02b
Ghala <ghalanmasi@icloud.com>
2023-11-17 04:56:01 +0300

    Version 2.1

--- myHomework.py
+++ myHomework.py
@@ -1,11 +1,17 @@
 star= '*' 
-i = 1
-while i < 5:
-    print(i*star)
-    i+=1
-    if i==5:
-        j=5
-        while j >= 1:
-            print(j*star)
-            j-=1
+try:
+    num=int(input("Enter number star:"))
+    i = 1
+    while i < num:
+        print(i*star)
+        i+=1
+        if i==num:
+            j=num
+            while j >= 1:
+                print(j*star)
+                j-=1
     
+except ValueError:
+    print("invalid entered try integer value")
+    
+  
